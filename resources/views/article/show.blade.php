58c13316cd6123e54e9e090eb96272eb4e181124
Gianni Lorusso <gianlor88@gmail.com>
2024-10-01 19:17:24 +0200

    mofiche su pagina show

--- resources/views/article/show.blade.php
+++ resources/views/article/show.blade.php
@@ -12,7 +12,7 @@
                 
                     <img src="{{ Storage::url($article->image)}}" class="card-img-top" alt="Immagine dell'articolo:{{$article->title}}">
             </div>
-            <div class="col-6 col-md-3 bg-body-tertiary border border-3 rounded">
+            <div class="col-6 col-md-3    ">
                 <h5 class="card-title text-center mt-2 fw-semibold">{{$article->title}}</h5>
                 <hr>
                 <p class="card-subtitle fst-italic text-center">{{$article->subtitle}}</p>
@@ -29,11 +29,11 @@
                         da {{$article->user->name}}</p>
             </div>
             <br>
-            <div class="col-12 justify-content-evenly text-center my-5 border-top">
+            <div class="col-8 justify-content-evenly text-center my-5 border-top">
 
                 <p class="mt-5">{{$article->body}}</p>
                 @if (Auth::user() && Auth::user()->is_revisor)
-                    <div class="container my-5">
+                    <div class="container mt-5">
                         <div class="row">
                             <div class="col-12 d-flex justify-content-evenly">
                                 <form action="{{route('revisor.acceptedArticle', $article)}}" method="post">
